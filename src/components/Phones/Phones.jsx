dfa49c9d9725317402ab6c3d2bde72b2f9ea67a9
Programming Hero <programming.hero1@gmail.com>
2023-09-16 16:39:07 -0500

    explore react awesome components

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { Audio, Grid } from 'react-loader-spinner'
 
 
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -23,13 +25,35 @@ const Phones = () => {
                     return obj;
                 })
                 console.log(phonesWithFakeData);
-                setPhones(phonesWithFakeData)
+                setPhones(phonesWithFakeData);
+                setLoading(false);
             });
 
     }, [])
 
     return (
         <div>
+            {loading && <div>
+                <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color='green'
+                    ariaLabel='three-dots-loading'
+                    wrapperStyle
+                    wrapperClass
+                />
+                <Grid
+  height="80"
+  width="80"
+  color="#4fa94d"
+  ariaLabel="grid-loading"
+  radius="12.5"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+/>
+            </div>}
             <h2 className="text-5xl">PHones: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
                 <Bar dataKey="price" fill="#8884d8" />
